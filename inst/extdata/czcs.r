f0abb5fc1eab4dca73a62ab2996e10bb16c7b1a9
Michael Sumner <mdsumner@gmail.com>
2016-01-08 16:34:01 +1100

    czcs

--- inst/extdata/czcs.r
+++ inst/extdata/czcs.r
@@ -1,6 +1,15 @@
 ##
 
 f <- "http://oceancolor.gsfc.nasa.gov/cgi/l3/C19843361984366.L3m_MO_CHL_chlor_a_9km.nc.png?sub=img"
+fname <- file.path("inst", "extdata", gsub("\\?sub=img", "", basename(f)))
+if (!file.exists(fname)) {
+  download.file(f, fname,  mode = "wb")
+}
 
-download.file(f, file.path("inst", "extdata", gsub("\\?sub=img", "", basename(f))), mode = "wb")
+library(raster)
+r <- brick(fname)
 
+llp <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
+gdalvrt(r, a_ullr = c(-180, 90, 180, -90), a_srs = llp)
+
+r <- brick(rgdal::readGDAL(gsub("png$", "vrt", fname)))
